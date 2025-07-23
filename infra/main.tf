21c09aab94df6b98ba5685a4137e76a61228cc8f
Ali ABBAS <ali.ibra.abbas@gmail.com>
2025-07-23 10:21:08 +0200

    First commit

---
+++ infra/main.tf
@@ -0,0 +1,131 @@
+resource "random_integer" "random" {
+  min = 1
+  max = 50000
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "${var.bucket_name}-${random_integer.random.result}"
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "sync_remote_website_content" {
+  for_each = fileset(var.sync_directories[0].local_source_directory, "**/*.*")
+
+  bucket = aws_s3_bucket.main.id
+  key    = "${var.sync_directories[0].s3_target_directory}/${each.value}"
+  source = "${var.sync_directories[0].local_source_directory}/${each.value}"
+  etag   = filemd5("${var.sync_directories[0].local_source_directory}/${each.value}")
+  content_type = try(
+    lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1]),
+    "binary/octet-stream"
+  )
+}
+
+# CloudFront Configuration
+
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "s3-oac"
+  description                       = "OAC for S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "main" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_id   = "s3Origin"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3Origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  tags = var.tags
+}
+
+
+# Bucket Policy for CloudFront Access
+
+resource "aws_s3_bucket_policy" "cloudfront_access" {
+  bucket = aws_s3_bucket.main.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.main.arn}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.main.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+output "cloudfront_url" {
+  value       = "https://${aws_cloudfront_distribution.main.domain_name}"
+  description = "L'URL publique pour accéder au site via CloudFront"
+}
